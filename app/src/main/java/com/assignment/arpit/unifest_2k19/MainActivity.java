5ca336eeada56008bafa0a44e501eca6fe45c09f
ARPIT <arpittrivedi2425@gmail.com>
2019-02-03 18:44:58 +0530

    Initial commit

---
+++ app/src/main/java/com/assignment/arpit/unifest_2k19/MainActivity.java
@@ -0,0 +1,20 @@
+package com.assignment.arpit.unifest_2k19;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}

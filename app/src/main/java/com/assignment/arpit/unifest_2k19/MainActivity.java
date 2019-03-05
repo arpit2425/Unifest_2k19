a7df8c5fe7efad9966626b1edb7462f2bd2051c8
ARPIT <arpittrivedi2425@gmail.com>
2019-03-06 03:26:46 +0530

    Main page

--- app/src/main/java/com/assignment/arpit/unifest_2k19/MainActivity.java
+++ app/src/main/java/com/assignment/arpit/unifest_2k19/MainActivity.java
@@ -7,13 +7,47 @@ import android.text.Html;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+ViewPager viewPager;
+adaptor adaptor;
+List<model> models;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        models=new ArrayList<>();
+        models.add(new model(R.drawable.events,"Events"));
+        models.add(new model(R.drawable.location,"Location"));
+        models.add(new model(R.drawable.spnsors,"Sponsers"));
+        models.add(new model(R.drawable.register,"Register"));
+        models.add(new model(R.drawable.developer,"Developer"));
+        models.add(new model(R.drawable.about,"About"));
+        adaptor=new adaptor(models,this);
+        viewPager=findViewById(R.id.viewpager);
+        viewPager.setAdapter(adaptor);
+        viewPager.setPadding(130,0,130,0);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
 
 
     }
